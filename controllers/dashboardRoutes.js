2915962d67f0151e8e9d81d46c84efdc99de475f
Karen Pion <karenlpion@gmail.com>
2021-08-02 02:27:09 -0400

    created post, user and comment models

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Post, User, Comment} = require('../models');
+
+router.get('/', async (req, res ) => {
+    try {
+        const
+    }
--- models/index.js
+++ models/index.js
@@ -1,5 +1,18 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
 
-module.exports = {
-    
-}
+
+
+
+
+
+
+
+
+module.exports = { 
+    User, 
+    Post, 
+    Comment
+};
